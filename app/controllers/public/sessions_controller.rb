a7cd130b722dd97b8045315da7d8e70325fbffdb
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-03 14:06:35 +0000

    [Add]is_deleted

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,14 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.vaild_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+      render :create
+    else
+      redirect_to new_customer_registration_path
+    end
+  end
 end
